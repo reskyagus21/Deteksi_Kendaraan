b0afe28fb7edfeda55d5b6b99fc5a8c47ac48c9d
Resky <144356073+reskyagus21@users.noreply.github.com>
2023-09-13 10:27:49 -0700

    Delete Api.py

--- Api.py
+++
@@ -1,4 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="ht2nPpDkIBwNpwA9hvM4")
-project = rf.workspace("skripsi-v7dmc").project("deteksi-pelanggaran")
-dataset = project.version(4).download("yolov5")
